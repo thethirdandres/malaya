a5480980dc71b8934641a9045f491cc452a61710
Andres <thethirdandres@gmail.com>
2022-08-11 22:27:31 +0800

    Script change

--- services/compose.js
+++ services/compose.js
@@ -6,20 +6,20 @@ module.exports = class {
     static composeMsgParam(payload, user){
         let msgParamList = [];
         switch (payload) {
-            case "PRE_GET_STARTED":
-            case "MENU_INITIAL_12":
-            case "MENU_INITIAL_16":
-            case "MENU_INITIAL_19":
-            case "MENU":
+            case "GET_STARTED":
+            case "AGE_12":
+            case "AGE_16":
+            case "AGE_19":
+            case "MENU_EXTENDED":
             case "BC_QUIZ":
                 msgParamList.push(user.firstName);
                 break;
              
             case "Q_FAMILY_PLANNING_NO":
                 if(user.pillStatus === "PROGESTIN"){
-                    msgParamList.push("PROGESTIN-ONLY PILLS!");
+                    msgParamList.push("PROGESTIN-ONLY PILLS");
                 } else {
-                    msgParamList.push("PILLS!");
+                    msgParamList.push("PILLS");
 
                 }
                 break;
